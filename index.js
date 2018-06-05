cdcac1ec301f65fa68d40c49bd697db9cabd4e33
lsaether <Lsaether@protonmail.com>
2018-06-05 12:10:36 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Config = require('src/config')
-const { Scanner } = require('src/scanner')
-const StatsDB = require('src/statsdb')
-const Wallet = require('src/wallet')
+const Config = require('./src/config')
+const { Scanner } = require('./src/scanner')
+const StatsDB = require('./src/statsdb')
+const Wallet = require('./src/wallet')
 const version = require('./package.json').version;
 
 module.exports = {
