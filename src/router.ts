d769008ad64018eefd5dbcdbac34eb7503d91b54
lsaether <Lsaether@protonmail.com>
2018-06-06 12:38:38 +0300

    router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,29 @@
+import BigNumber = require('bignumber.js');
+
 import Config from './config';
+
+enum Status {
+    BeforeClaimWindow,
+    ClaimWindow,
+    FreezePeriod,
+    ExecutionWindow,
+    Executed,
+}
 
 export default class Router {
     config: Config;
+    txRequestStates: Object;
+
+    transitions: Object;
 
     constructor(config: Config) {
         this.config = config;
+
+        this.transitions[Status.BeforeClaimWindow] = this.beforeClaimWindow();
+        this.transitions[Status.ClaimWindow] = this.claimWindow();
+        this.transitions[Status.FreezePeriod] = this.freezePeriod();
+        this.transitions[Status.ExecutionWindow] = this.executionWindow();
+        this.transitions[Status.Executed] = this.executed();
     }
 
     isLocalClaim(txRequest) {
@@ -25,7 +44,29 @@ export default class Router {
         return localClaim;
     }
 
-    isProfitableClaim(txRequest) {
-        
+    async isProfitableClaim(txRequest) {
+        const claimPaymentModifier = await txRequest.claimPaymentModifier();
+        const paymentWhenClaimed = txRequest.bounty.times(
+            claimPaymentModifier,
+        ).dividedToIntegerBy(100);
+
+        // TODO
+
     }
+
+    // TODO do not return void
+    async route(txRequest): Promise<void> {
+        let status: Status = this.txRequestStates[txRequest.address] || Status.BeforeClaimWindow;
+        let nextStatus: Status = this.transitions[status]();
+
+        while (nextStatus !== status) {
+            status = nextStatus;
+            nextStatus = this.transitions[status]
+        }
+
+        this.txRequestStates[txRequest.address] = nextStatus;
+        return
+    }
+
+
 }
