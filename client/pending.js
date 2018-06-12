492347b7e38d14f3d30198d2c558693d6d492916
Anthony <adibas03@ymail.com>
2018-06-12 09:55:32 +0100

    begin new implementation

--- client/pending.js
+++ client/pending.js
@@ -2,6 +2,7 @@
  * Uses the Parity specific RPC request `parity_pendingTransactions` to search
  * for pending transactions in the transaction pool.
  * @param {TransactionRequest} txRequest
+ * @param {string} type (optional) Type of pending request: claim,execute.
  * @returns {Promise<boolean>} True if a pending transaction to this address exists.
  */
 const hasPendingParity = async (conf, txRequest, type) => {
@@ -18,7 +19,7 @@ const hasPendingParity = async (conf, txRequest, type) => {
       (err, res) => {
         if (err) reject(err)
 
-        const hasTx = res && res.result && !!res.result.filter(tx => tx.to === txRequest.address).length
+        const hasTx = res && res.result && !!res.result.find(tx => tx.to === txRequest.address)
         resolve(hasTx)
       }
     )
@@ -29,9 +30,78 @@ const hasPendingParity = async (conf, txRequest, type) => {
  * Uses the Geth specific RPC request `txpool_content` to search
  * for pending transactions in the transaction pool.
  * @param {TransactionRequest} txRequest
+ * @param {string} type (optional) Type of pending request: claim,execute.
+ * @param {bool} checkGasPrice (default: true) Check if transaction's gasPrice is sufficient for Network when (type: claim).
+ * @returns {Promise<object>} Transaction, if a pending transaction to this address exists.
+ */
+const hasPendingGeth = (conf, txRequest, type, checkGasPrice = true) => {
+  const provider = conf.web3.currentProvider
+
+  return new Promise((resolve, reject) => {
+    provider.send(
+      {
+        jsonrpc: '2.0',
+        method: 'txpool_content',
+        params: [],
+        id: 0o7,
+      },
+      (err, res) => {
+        if (err) reject(err)
+        for (const account in res.result.pending) {
+          for (const nonce in res.result.pending[account]) {
+            if (res.result.pending[account][nonce].to === txRequest.address) {
+              resolve(res.result.pending[account][nonce])
+            }
+          }
+        }
+        resolve(false)
+      }
+    )
+  })
+}
+
+/**
+ * Uses the Geth specific RPC request `txpool_content` to search
+ * for pending transactions in the transaction pool.
+ * @param {TransactionRequest} txRequest
+ * @param {string} type (optional) Type of pending request: claim,execute.
+ * @param {bool} checkGasPrice (default: true) Check if transaction's gasPrice is sufficient for Network when (type: claim). 
+ * @returns {Promise<object>} Transaction, if a pending transaction to this address exists.
+ */
+const hasValidGasPrice = (conf, txRequest, type, checkGasPrice = true) => {
+  const provider = conf.web3.currentProvider
+
+  return new Promise((resolve, reject) => {
+    provider.send(
+      {
+        jsonrpc: '2.0',
+        method: 'txpool_content',
+        params: [],
+        id: 0o7,
+      },
+      (err, res) => {
+        if (err) reject(err)
+        for (const account in res.result.pending) {
+          for (const nonce in res.result.pending[account]) {
+            if (res.result.pending[account][nonce].to === txRequest.address) {
+              resolve(true)
+            }
+          }
+        }
+        resolve(false)
+      }
+    )
+  })
+}
+
+/**
+ * Uses the Geth specific RPC request `txpool_content` to search
+ * for pending transactions in the transaction pool.
+ * @param {TransactionReceipt} transaction
+ * @param {string} type Type of pending request: claim,execute.
  * @returns {Promise<boolean>} True if a pending transaction to this address exists.
  */
-const hasPendingGeth = (conf, txRequest, type) => {
+const isOfType = (transaction, type) => {
   const provider = conf.web3.currentProvider
 
   return new Promise((resolve, reject) => {
@@ -62,9 +132,10 @@ const hasPendingGeth = (conf, txRequest, type) => {
  * a TransactionRequest has a pending transaction in the transaction pool.
  * @param {Config} conf Config object.
  * @param {TransactionRequest} txRequest Transaction Request object to check.
+ * @param {bool} checkGasPrice (default: true) Check if transaction's gasPrice is sufficient for Network when (type: claim).
  * @param {string} type (optional) Type of pending request: claim,execute.
  */
-const hasPending = (conf, txRequest, type) => {
+const hasPending = (conf, txRequest, type, checkGasPrice = true) => {
   if (conf.client == 'parity') {
     return hasPendingParity(conf, txRequest, type)
   } else if (conf.client == 'geth') {
