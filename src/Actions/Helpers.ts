fcf725d2d15b45eb6789855f8cf7f36aa6a7faf3
Daniel Kmak <kuzirashi@interia.pl>
2018-06-21 11:57:51 +0200

    Format.

--- src/Actions/Helpers.ts
+++ src/Actions/Helpers.ts
@@ -8,8 +8,8 @@ const isExecuted = (receipt: any) => {
   return false;
 };
 
-const isTransactionStatusSuccessful = (status : string | number) => {
-  return status && ([1, '0x1', '0x01'].indexOf(status) !== -1);
-}
+const isTransactionStatusSuccessful = (status: string | number) => {
+  return status && [1, '0x1', '0x01'].indexOf(status) !== -1;
+};
 
 export { isExecuted, isTransactionStatusSuccessful };
