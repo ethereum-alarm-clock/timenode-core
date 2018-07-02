ac3fc5611afffb8499027f622012c0639c0be484
Bagaric <joseph@josephit.com>
2018-07-02 18:48:20 +0200

    Add unit tests for StatsDB

--- test/unit/UnitTestEconomicStrategy.ts
+++ test/unit/UnitTestEconomicStrategy.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import { expect, assert } from 'chai';
+import { assert } from 'chai';
 import { Config } from '../../src/index';
 import { mockConfig, MockTxRequest } from '../helpers';
 import { shouldClaimTx } from '../../src/EconomicStrategy';
