77ee79e2a8437ad720e9955240bc73cad9e302e6
Bagaric <joseph@josephit.com>
2018-07-02 17:36:53 +0200

    Rename

--- test/unit/UnitTestEconomicStrategy.ts
+++ test/unit/UnitTestEconomicStrategy.ts
@@ -18,31 +18,31 @@ describe('Economic Strategy Tests', () => {
   describe('shouldClaimTx()', () => {
     it('returns true if economic strategy not set', async () => {
       config.economicStrategy = null;
-      const result = await shouldClaimTx(txTimestamp, config);
-      assert.isTrue(result);
+      const shouldClaim = await shouldClaimTx(txTimestamp, config);
+      assert.isTrue(shouldClaim);
     });
 
     it('returns true if economic strategy is zeroed', async () => {
-      const result = await shouldClaimTx(txTimestamp, config);
-      assert.isTrue(result);
+      const shouldClaim = await shouldClaimTx(txTimestamp, config);
+      assert.isTrue(shouldClaim);
     });
 
     it('returns false if transaction exceeds maxDeposit', async () => {
       config.economicStrategy.maxDeposit = new BigNumber(1);
-      const result = await shouldClaimTx(txTimestamp, config);
-      assert.isFalse(result);
+      const shouldClaim = await shouldClaimTx(txTimestamp, config);
+      assert.isFalse(shouldClaim);
     });
 
     it('returns false if balance below minBalance', async () => {
       config.economicStrategy.minBalance = new BigNumber(config.web3.toWei(101, 'ether'));
-      const result = await shouldClaimTx(txTimestamp, config);
-      assert.isFalse(result);
+      const shouldClaim = await shouldClaimTx(txTimestamp, config);
+      assert.isFalse(shouldClaim);
     });
 
     it('returns false if reward lower than minProfitability', async () => {
       config.economicStrategy.minProfitability = new BigNumber(config.web3.toWei(100, 'ether'));
-      const result = await shouldClaimTx(txTimestamp, config);
-      assert.isFalse(result);
+      const shouldClaim = await shouldClaimTx(txTimestamp, config);
+      assert.isFalse(shouldClaim);
     });
   });
 });
