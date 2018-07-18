68615e5251ab6c188eaddcfd9f890eafd9c234c5
Bagaric <joseph@josephit.com>
2018-07-18 16:16:30 +0200

    Remove export StatsDB

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import Config from './Config';
 import TimeNode from './TimeNode';
 import Wallet from './Wallet';
-import { StatsDB } from './Stats';
 
 import 'babel-polyfill';
 
-export { Config, TimeNode, Wallet, StatsDB };
+export { Config, TimeNode, Wallet };
