3a424f4bbc9827b7c56eeaf31c0d06cfb11a724d
Bagaric <joseph@josephit.com>
2018-07-23 16:13:56 +0200

    Add tests for action helpers

--- src/Actions/Helpers.ts
+++ src/Actions/Helpers.ts
@@ -1,15 +1,18 @@
+const EXECUTED_EVENT = '0x3e504bb8b225ad41f613b0c3c4205cdd752d1615b4d77cd1773417282fcfb5d9';
+
 const isExecuted = (receipt: any) => {
   if (receipt) {
-    const executedEvent =
-      '0x3e504bb8b225ad41f613b0c3c4205cdd752d1615b4d77cd1773417282fcfb5d9';
-    return receipt.logs[0].topics.indexOf(executedEvent) > -1;
+    return receipt.logs[0].topics.indexOf(EXECUTED_EVENT) > -1;
   }
 
   return false;
 };
 
 const isTransactionStatusSuccessful = (status: string | number) => {
-  return status && [1, '0x1', '0x01'].indexOf(status) !== -1;
+  if (status) {
+    return [1, '0x1', '0x01'].indexOf(status) !== -1;
+  }
+  return false;
 };
 
-export { isExecuted, isTransactionStatusSuccessful };
+export { isExecuted, isTransactionStatusSuccessful, EXECUTED_EVENT };
