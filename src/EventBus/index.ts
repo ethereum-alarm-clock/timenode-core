3e8635cb753cc3de5b482e1f7d884aed59aff487
lsaether <Lsaether@protonmail.com>
2018-07-24 12:18:16 +0200

    naive eventbus

---
+++ src/EventBus/index.ts
@@ -0,0 +1,35 @@
+import * as EventEmitter from 'events';
+
+interface IListener {
+  onEvent(a: string): any;
+}
+
+interface ISubscribers {
+  eventTopic: string[];
+}
+
+export default class EventBus {
+  public subscribers: {};
+  public eventEmitter: any;
+
+  constructor() {
+    this.eventEmitter = new EventEmitter();
+  }
+
+  public register(eventTopic: string): boolean {
+    this.eventEmitter.on(eventTopic, () => {
+      this.pushEvents(eventTopic);
+    });
+  }
+
+  public subscribe(eventTopic: string, listener: any): boolean {
+    this.subscribers[eventTopic].push(listener);
+    return true;
+  }
+
+  private pushEvents(eventTopic: string) {
+    this.subscribers[eventTopic].forEach((element: IListener) => {
+      element.onEvent(eventTopic);
+    });
+  }
+}
