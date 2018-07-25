1fef8dcfd2b5e68c110b410e53120cdccddb90b7
Joseph Bagaric <joseph@josephit.com>
2018-07-25 13:39:47 +0200

    More tests (#152)
    * Logger tests
    
    * Scanner and stats tests
    
    * More wallet tests
    
    * TimeNode tests
    
    * Function naming

--- test/unit/UnitTestTimeNode.ts
+++ test/unit/UnitTestTimeNode.ts
@@ -1,6 +1,7 @@
 import { expect, assert } from 'chai';
 import { TimeNode, Config } from '../../src/index';
 import { mockConfig } from '../helpers';
+import { BigNumber } from 'bignumber.js';
 
 describe('TimeNode Unit Tests', () => {
   const config: Config = mockConfig();
@@ -31,16 +32,48 @@ describe('TimeNode Unit Tests', () => {
   });
 
   describe('startClaiming()', () => {
-    it('returns true when started claiming', async () => {
+    it('returns true when started claiming', () => {
       assert.isTrue(timenode.startClaiming());
       assert.isTrue(timenode.config.claiming);
     });
   });
 
   describe('stopClaiming()', () => {
-    it('returns false when stopped claiming', async () => {
+    it('returns false when stopped claiming', () => {
       assert.isFalse(timenode.stopClaiming());
       assert.isFalse(timenode.config.claiming);
+    });
+  });
+
+  describe('logNetwork()', () => {
+    xit('logs the network id', () => {
+      let networkLogged = false;
+
+      timenode.config.logger.info = (msg: string) => {
+        networkLogged = true;
+      };
+
+      timenode.logNetwork();
+      assert.isTrue(networkLogged);
+    });
+  });
+
+  describe('getClaimedNotExecutedTransactions()', () => {
+    it('returns 0 when no transactions', () => {
+      const txs = timenode.getClaimedNotExecutedTransactions();
+      assert.equal(txs.length, 0);
+    });
+
+    it('returns a transaction', () => {
+      const tx = {
+        claimedBy: config.wallet.getAddresses()[0],
+        wasCalled: false,
+        windowStart: new BigNumber(10000)
+      };
+      config.cache.set('tx', tx);
+
+      const txs = timenode.getClaimedNotExecutedTransactions();
+      assert.equal(txs.length, 1);
     });
   });
 });
