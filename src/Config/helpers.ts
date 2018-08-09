7b67cbf8b33164640e46661059d0953102f3a1d4
Adibas03 <adibas03@ymail.com>
2018-08-09 10:34:58 +0100

    Fix webSocket provider

--- src/Config/helpers.ts
+++ src/Config/helpers.ts
@@ -2,13 +2,13 @@ import * as Web3 from 'web3';
 import * as Web3WsProvider from 'web3-providers-ws';
 
 const getWeb3FromProviderUrl = (providerUrl: string) => {
-  let provider;
+  let provider: any;
 
   if (providerUrl.includes('http://') || providerUrl.includes('https://')) {
     provider = new Web3.providers.HttpProvider(providerUrl);
   } else if (providerUrl.includes('ws://') || providerUrl.includes('wss://')) {
     provider = new Web3WsProvider(providerUrl);
-    provider.__proto__.sendAsync = provider.__proto__.send;
+    provider.__proto__.sendAsync = provider.__proto__.sendAsync || provider.__proto__.send;  
   }
 
   return new Web3(provider);
