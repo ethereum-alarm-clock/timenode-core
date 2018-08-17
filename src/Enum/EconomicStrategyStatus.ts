ee85208f011313c55073dc9448578cacdba407cf
Bagaric <joseph@josephit.com>
2018-08-17 15:28:07 +0200

    Remove unknown

--- src/Enum/EconomicStrategyStatus.ts
+++ src/Enum/EconomicStrategyStatus.ts
@@ -2,6 +2,5 @@ export enum EconomicStrategyStatus {
   NOT_PROFITABLE = 'Transaction not profitable.',
   INSUFFICIENT_BALANCE = 'Not enough balance to claim.',
   CLAIM = 'Transaction can be claimed.',
-  DEPOSIT_TOO_HIGH = 'The transaction deposit is too high.',
-  UNKNOWN = 'UNKNOWN.'
+  DEPOSIT_TOO_HIGH = 'The transaction deposit is too high.'
 }
