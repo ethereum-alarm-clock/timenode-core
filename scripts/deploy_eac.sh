251cf0bb591056a4fa5c0803d7a5448b7ea00a64
Bagaric <joseph@josephit.com>
2018-08-17 22:18:16 +0200

    Fix deployment script

--- scripts/deploy_eac.sh
+++ scripts/deploy_eac.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Deploying contracts..."
-cd node_modules/@ethereum-alarm-clock/contracts
+cd node_modules/eac.js-lib/node_modules/@ethereum-alarm-clock/contracts
 truffle network --clean
 truffle migrate --reset
-cd ../../../node_modules/eac.js-lib/
+cd ../../../
 
 echo "Moving the generated contract files..."
 rm -Rfv lib/build/*
-cp -Rfv ../@ethereum-alarm-clock/contracts/build/* lib/build/
-cp -fv ../@ethereum-alarm-clock/contracts/package.json lib/build/ethereum-alarm-clock.json
+cp -Rfv node_modules/@ethereum-alarm-clock/contracts/build/* lib/build/
+cp -fv node_modules/@ethereum-alarm-clock/contracts/package.json lib/build/ethereum-alarm-clock.json
 
 node ./extractContractsInfo.js development
 mv -fv contracts.json lib/assets/development.json || true
