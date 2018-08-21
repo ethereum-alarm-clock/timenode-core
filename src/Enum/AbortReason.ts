40439cfa6a5dbc856bb96582f25cb0f93491fb4c
Piotr Kosiński <piotrkosinski@gmail.com>
2018-08-21 13:27:20 +0200

    Rm abortreson.ts

--- src/Enum/AbortReason.ts
+++
@@ -1,9 +0,0 @@
-enum AbortReason {
-  WasCancelled, //0
-  AlreadyCalled, //1
-  BeforeCallWindow, //2
-  AfterCallWindow, //3
-  ReservedForClaimer, //4
-  InsufficientGas, //5
-  TooLowGasPrice //6
-}
