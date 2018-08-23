5d291fc9a2fc25595cfce911658f22bba6654247
Piotr Kosiński <piotrkosinski@gmail.com>
2018-08-23 08:47:54 +0200

    rm babel-polyfill

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ import TimeNode from './TimeNode';
 import { Wallet } from './Wallet';
 import version from './Version';
 
-import 'babel-polyfill';
-
-export { Config, TimeNode, Wallet, version, };
+export { Config, TimeNode, Wallet, version };
