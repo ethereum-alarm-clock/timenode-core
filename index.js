ba361bd30cf9b0b0d5942d856dfefa66670082b7
Daniel Kmak <kuzirashi@interia.pl>
2018-09-03 11:56:59 +0200

    Fix export.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { Config, TimeNode, StatsDB, Wallet, version } = require('./built/src');
+const { Config, TimeNode, StatsDB, Wallet, version, W3Util } = require('./built/src');
 
 module.exports = {
   Config,
   TimeNode,
   StatsDB,
+  W3Util,
   Wallet,
   version
 };
