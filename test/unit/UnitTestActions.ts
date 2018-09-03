0b99050de0a8fc531632fd6a92eb6edde9577651
Piotr Kosiński <piotrkosinski@gmail.com>
2018-09-03 09:43:43 +0200

    Added AbortReason logging

--- test/unit/UnitTestActions.ts
+++ test/unit/UnitTestActions.ts
@@ -6,9 +6,12 @@ import { shortenAddress } from '../../src/Actions/Actions';
 import {
   isExecuted,
   EXECUTED_EVENT,
-  isTransactionStatusSuccessful
+  isTransactionStatusSuccessful,
+  ABORTED_EVENT,
+  isAborted,
+  getAbortedExecuteStatus
 } from '../../src/Actions/Helpers';
-import { ClaimStatus } from '../../src/Enum';
+import { ClaimStatus, ExecuteStatus } from '../../src/Enum';
 
 describe('shortenAddress()', () => {
   const address = '0x487a54e1d033db51c8ee8c03edac2a0f8a6892c6';
@@ -93,6 +96,156 @@ describe('Actions Helpers Unit Tests', () => {
       assert.isFalse(isTransactionStatusSuccessful(2));
       assert.isFalse(isTransactionStatusSuccessful('2'));
       assert.isFalse(isTransactionStatusSuccessful('0x02'));
+    });
+  });
+
+  describe('isAborted()', () => {
+    it('returns true when executed event was aborted', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT]
+          }
+        ]
+      };
+      assert.isTrue(isAborted(receipt));
+    });
+  });
+
+  describe('getAbortedExecuteStatus()', () => {
+    it('returns ExecuteStatus.ABORTED_WAS_CANCELLED when AbortReason.WasCancelled', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000000'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_WAS_CANCELLED, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_ALREADY_CALLED when AbortReason.AlreadyCalled', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000001'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_ALREADY_CALLED, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_BEFORE_CALL_WINDOW when AbortReason.BeforeCallWindow', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000002'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_BEFORE_CALL_WINDOW, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_AFTER_CALL_WINDOW when AbortReason.AfterCallWindow', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000003'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_AFTER_CALL_WINDOW, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_RESERVED_FOR_CLAIMER when AbortReason.ReservedForClaimer', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000004'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_RESERVED_FOR_CLAIMER, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_INSUFFICIENT_GAS when AbortReason.InsufficientGas', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000005'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_INSUFFICIENT_GAS, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_TOO_LOW_GAS_PRICE when AbortReason.TooLowGasPrice', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000006'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_TOO_LOW_GAS_PRICE, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_UNKNOWN when unknown reason appeared', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: '0x0000000000000000000000000000000000000000000000000000000000000008'
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_UNKNOWN, executeStatus);
+    });
+
+    it('returns ExecuteStatus.ABORTED_UNKNOWN when no data found', () => {
+      const receipt = {
+        logs: [
+          {
+            topics: [ABORTED_EVENT],
+            data: ''
+          }
+        ]
+      };
+
+      const executeStatus = getAbortedExecuteStatus(receipt);
+
+      assert.equal(ExecuteStatus.ABORTED_UNKNOWN, executeStatus);
     });
   });
 });
