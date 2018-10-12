42317c01c293c6b040aa17fedfd57af59bfbb7fd
lsaether <Lsaether@protonmail.com>
2018-10-12 14:19:52 +0200

    add sane minimum tests

--- test/unit/UnitTestGasEstimation.ts
+++ test/unit/UnitTestGasEstimation.ts
@@ -1,15 +1,24 @@
 import { assert } from 'chai';
-
+import { Config } from '../../src/index';
+import { mockConfig } from '../helpers';
 import { BlockScaleFetchingService, EthGasStationFetchingService } from '../../src/GasEstimation';
 import BigNumber from 'bignumber.js';
 
-describe('Gas Price Estimation Tests', () => {
+describe('Gas Price Estimation Tests', async () => {
+  const config: Config = await mockConfig();
+
+  const gasPriceValues = ['average', 'fast', 'fastest', 'safeLow'];
+
   describe('BlockScaleFetchingService', async () => {
     const blockscale = new BlockScaleFetchingService();
     const result = await blockscale.fetchGasPrice();
 
     Object.keys(result).forEach(field => {
       assert.isTrue(result[field] instanceof BigNumber, `${field} is not a BigNumber!`);
+    });
+
+    gasPriceValues.forEach(value => {
+      assert.isTrue(result[value] > config.web3.toWei('0.05', 'gwei'));
     });
 
     assert.isTrue(result.safeLow <= result.average);
@@ -24,6 +33,10 @@ describe('Gas Price Estimation Tests', () => {
 
     Object.keys(result).forEach(field => {
       assert.isTrue(result[field] instanceof BigNumber, `${field} is not a BigNumber!`);
+    });
+
+    gasPriceValues.forEach(value => {
+      assert.isTrue(result[value] > config.web3.toWei('0.05', 'gwei'));
     });
 
     assert.isTrue(result.safeLow <= result.average);
