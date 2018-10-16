1fcb285b483c72f5ed7b2d9c5fba160bbcf32834
Piotr Kosiński <piotrkosinski@gmail.com>
2018-10-16 17:39:53 +0200

    Additional shifting test

--- test/unit/UnitTestBuckets.ts
+++ test/unit/UnitTestBuckets.ts
@@ -50,6 +50,38 @@ describe('WatchableBucket', () => {
     watchableBucketFactory.verifyAll();
   });
 
+  it('should shift buckets', async () => {
+    const bucketsPairs = TypeMoq.Mock.ofType<IBuckets>();
+    bucketsPairs
+      .setup(b => b.currentBuckets)
+      .returns(() => ({ timestampBucket: 1, blockBucket: 10000 }));
+
+    const bucketsPairs2 = TypeMoq.Mock.ofType<IBuckets>();
+    bucketsPairs
+      .setup(b => b.currentBuckets)
+      .returns(() => ({ timestampBucket: 2, blockBucket: 10001 }));
+
+    const watchableBucket = TypeMoq.Mock.ofType<WatchableBucket>();
+    watchableBucket.setup(w => w.watch()).verifiable(TypeMoq.Times.exactly(4));
+    watchableBucket.setup(w => w.stop()).verifiable(TypeMoq.Times.exactly(1));
+    watchableBucket.setup(w => w.equals(TypeMoq.It.isAny())).returns(() => false);
+    watchableBucket.setup((x: any) => x.then).returns(() => undefined);
+
+    const watchableBucketFactory = TypeMoq.Mock.ofType<WatchableBucketFactory>();
+    watchableBucketFactory
+      .setup(r => r.create(TypeMoq.It.isAny(), TypeMoq.It.isAny()))
+      .returns(async () => watchableBucket.object)
+      .verifiable(TypeMoq.Times.exactly(4));
+
+    const buckets = new Buckets(watchableBucketFactory.object);
+
+    await buckets.update(bucketsPairs.object, null);
+    await buckets.update(bucketsPairs2.object, null);
+
+    watchableBucket.verifyAll();
+    watchableBucketFactory.verifyAll();
+  });
+
   it('should stop all buckets when requested', async () => {
     const bucketsPairs = TypeMoq.Mock.ofType<IBuckets>();
 
