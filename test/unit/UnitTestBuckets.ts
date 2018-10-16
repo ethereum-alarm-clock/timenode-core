c41bb4f67a16b7ff26101751b9e42d92c6f1eac1
Piotr Kosiński <piotrkosinski@gmail.com>
2018-10-16 17:53:49 +0200

    A bit more detailed test for shifting

--- test/unit/UnitTestBuckets.ts
+++ test/unit/UnitTestBuckets.ts
@@ -51,34 +51,62 @@ describe('WatchableBucket', () => {
   });
 
   it('should shift buckets', async () => {
+    const currentBuckets = { timestampBucket: 0, blockBucket: 10000 };
+    const currentBuckets2 = { timestampBucket: 2, blockBucket: 10001 };
+    const afterNextBuckets = { timestampBucket: 3, blockBucket: 10002 };
+
     const bucketsPairs = TypeMoq.Mock.ofType<IBuckets>();
-    bucketsPairs
-      .setup(b => b.currentBuckets)
-      .returns(() => ({ timestampBucket: 1, blockBucket: 10000 }));
+    bucketsPairs.setup(b => b.currentBuckets).returns(() => currentBuckets);
 
     const bucketsPairs2 = TypeMoq.Mock.ofType<IBuckets>();
-    bucketsPairs
-      .setup(b => b.currentBuckets)
-      .returns(() => ({ timestampBucket: 2, blockBucket: 10001 }));
+    bucketsPairs.setup(b => b.currentBuckets).returns(() => currentBuckets2);
+    bucketsPairs2.setup(b => b.afterNextBuckets).returns(() => afterNextBuckets);
 
-    const watchableBucket = TypeMoq.Mock.ofType<WatchableBucket>();
-    watchableBucket.setup(w => w.watch()).verifiable(TypeMoq.Times.exactly(4));
-    watchableBucket.setup(w => w.stop()).verifiable(TypeMoq.Times.exactly(1));
-    watchableBucket.setup(w => w.equals(TypeMoq.It.isAny())).returns(() => false);
-    watchableBucket.setup((x: any) => x.then).returns(() => undefined);
+    const watchableBucketForCurrentBuckets = TypeMoq.Mock.ofType<WatchableBucket>();
+    watchableBucketForCurrentBuckets.setup(w => w.watch()).verifiable(TypeMoq.Times.exactly(1));
+    watchableBucketForCurrentBuckets.setup(w => w.stop()).verifiable(TypeMoq.Times.exactly(1));
+    watchableBucketForCurrentBuckets.setup(w => w.equals(TypeMoq.It.isAny())).returns(() => false);
+    watchableBucketForCurrentBuckets.setup((x: any) => x.then).returns(() => undefined);
+
+    const watchableBucketForCurrentBuckets2 = TypeMoq.Mock.ofType<WatchableBucket>();
+    watchableBucketForCurrentBuckets2.setup(w => w.watch()).verifiable(TypeMoq.Times.exactly(0));
+    watchableBucketForCurrentBuckets2.setup(w => w.stop()).verifiable(TypeMoq.Times.exactly(0));
+    watchableBucketForCurrentBuckets2.setup((x: any) => x.then).returns(() => undefined);
+
+    const watchableBucketForAfterNextBuckets = TypeMoq.Mock.ofType<WatchableBucket>();
+    watchableBucketForAfterNextBuckets.setup(w => w.watch()).verifiable(TypeMoq.Times.exactly(1));
+    watchableBucketForAfterNextBuckets.setup(w => w.stop()).verifiable(TypeMoq.Times.exactly(0));
+    watchableBucketForAfterNextBuckets.setup((x: any) => x.then).returns(() => undefined);
+
+    const watchableBucketDefault = TypeMoq.Mock.ofType<WatchableBucket>();
+    watchableBucketDefault.setup((x: any) => x.then).returns(() => undefined);
 
     const watchableBucketFactory = TypeMoq.Mock.ofType<WatchableBucketFactory>();
     watchableBucketFactory
+      .setup(r => r.create(currentBuckets, TypeMoq.It.isAny()))
+      .returns(async () => watchableBucketForCurrentBuckets.object)
+      .verifiable(TypeMoq.Times.exactly(1));
+
+    watchableBucketFactory
+      .setup(r => r.create(currentBuckets2, TypeMoq.It.isAny()))
+      .returns(async () => watchableBucketForCurrentBuckets2.object)
+      .verifiable(TypeMoq.Times.exactly(0));
+
+    watchableBucketFactory
+      .setup(r => r.create(afterNextBuckets, TypeMoq.It.isAny()))
+      .returns(async () => watchableBucketForAfterNextBuckets.object)
+      .verifiable(TypeMoq.Times.exactly(1));
+
+    watchableBucketFactory
       .setup(r => r.create(TypeMoq.It.isAny(), TypeMoq.It.isAny()))
-      .returns(async () => watchableBucket.object)
-      .verifiable(TypeMoq.Times.exactly(4));
+      .returns(async () => watchableBucketDefault.object);
 
     const buckets = new Buckets(watchableBucketFactory.object);
 
     await buckets.update(bucketsPairs.object, null);
     await buckets.update(bucketsPairs2.object, null);
 
-    watchableBucket.verifyAll();
+    watchableBucketForCurrentBuckets.verifyAll();
     watchableBucketFactory.verifyAll();
   });
 
