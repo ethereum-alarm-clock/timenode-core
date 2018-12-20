ff76cb84d4aaade66fbee39f4db4eaa6cee05d0e
Bagaric <joseph@josephit.com>
2018-12-20 11:37:09 +0100

    Remove unnecessary calcEndowment, now fetched from lib

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -2,4 +2,3 @@ export { mockTxRequest, mockTxStatus } from './MockTxRequest';
 export { mockConfig, PRIVATE_KEY } from './mockConfig';
 export { createWallet, createWalletKeystore } from './createWallet';
 export { providerUrl } from './network';
-export { calcEndowment } from './calcEndowment';
