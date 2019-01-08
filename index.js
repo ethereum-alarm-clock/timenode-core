22da68ab1da50497e048e88a5d45492c0fc55e62
Bagaric <joseph@josephit.com>
2019-01-08 13:36:53 +0100

    Remove W3Util from exports

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const { Config, TimeNode, StatsDB, Wallet, version, W3Util } = require('./built/src');
+const { Config, TimeNode, StatsDB, Wallet, version } = require('./built/src');
 
 module.exports = {
   Config,
   TimeNode,
   StatsDB,
-  W3Util,
   Wallet,
   version
 };
