0054950873d582e315d0ed63d5fd05a73a0e772f
Piotr Kosiński <piotrkosinski@gmail.com>
2019-01-24 14:17:08 +0100

    Start from latest known block. kovan configuration

--- src/TxPool/DirectTxPool.ts
+++ src/TxPool/DirectTxPool.ts
@@ -14,12 +14,45 @@ export default class DirectTxPool implements ITxPool {
   private static ExecuteData = '61461954';
   private static ClaimData = '4e71d92d';
   private static MaxPeers = 25;
+  private static NetworkStatusMessage = new Map<Networks, any>([
+    [
+      Networks.Mainnet,
+      {
+        networkId: Networks.Mainnet,
+        td: devp2p._util.int2buffer(17179869184), // total difficulty in genesis block
+        bestHash: Buffer.from(
+          'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
+          'hex'
+        ),
+        genesisHash: Buffer.from(
+          'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
+          'hex'
+        )
+      }
+    ],
+    [
+      Networks.Kovan,
+      {
+        networkId: Networks.Kovan,
+        td: devp2p._util.int2buffer(131072), // total difficulty in genesis block
+        bestHash: Buffer.from(
+          'a3c565fc15c7478862d50ccd6561e3c06b24cc509bf388941c25ea985ce32cb9',
+          'hex'
+        ),
+        genesisHash: Buffer.from(
+          'a3c565fc15c7478862d50ccd6561e3c06b24cc509bf388941c25ea985ce32cb9',
+          'hex'
+        )
+      }
+    ]
+  ]);
 
   public pool: Map<string, ITxPoolTxDetails> = new Map<string, ITxPoolTxDetails>();
   private isRunning: boolean = false;
   private web3: Web3;
   private privateKey: Buffer;
   private logger: ILogger;
+  private chainId: Networks;
 
   private dpt: any;
   private rlpx: any;
@@ -38,7 +71,9 @@ export default class DirectTxPool implements ITxPool {
 
   public async start() {
     const chainId = await this.web3.eth.net.getId();
-    if (!this.isRunning && chainId === Networks.Mainnet) {
+
+    if (!this.isRunning && this.isChainSupported(chainId)) {
+      this.chainId = chainId;
       this.startListening();
     }
   }
@@ -47,18 +82,52 @@ export default class DirectTxPool implements ITxPool {
     clearInterval(this.status);
   }
 
+  private isChainSupported(chainId: number) {
+    return chainId === Networks.Mainnet || chainId === Networks.Kovan;
+  }
+
   private get bootNodes() {
-    return bootstrapNodes
-      .filter((node: any) => {
-        return node.chainId === Networks.Mainnet;
-      })
-      .map((node: any) => {
-        return {
-          address: node.ip,
-          udpPort: node.port,
-          tcpPort: node.port
-        };
-      });
+    if (this.chainId === Networks.Mainnet) {
+      return bootstrapNodes
+        .filter((node: any) => {
+          return node.chainId === Networks.Mainnet;
+        })
+        .map((node: any) => {
+          return {
+            address: node.ip,
+            udpPort: node.port,
+            tcpPort: node.port
+          };
+        });
+    } else if (this.chainId === Networks.Kovan) {
+      return [
+        {
+          address: '40.71.221.215',
+          udpPort: 30303,
+          tcpPort: 30303
+        },
+        {
+          address: '52.166.117.77',
+          udpPort: 30303,
+          tcpPort: 30303
+        },
+        {
+          address: '52.165.239.18',
+          udpPort: 30303,
+          tcpPort: 30303
+        },
+        {
+          address: '52.243.47.56',
+          udpPort: 30303,
+          tcpPort: 30303
+        },
+        {
+          address: '40.68.248.100',
+          udpPort: 30303,
+          tcpPort: 30303
+        }
+      ];
+    }
   }
 
   private bootstrap(bootNodes: any[]) {
@@ -100,19 +169,24 @@ export default class DirectTxPool implements ITxPool {
     });
   }
 
-  private sendStatus(eth: any) {
-    eth.sendStatus({
-      networkId: Networks.Mainnet,
-      td: devp2p._util.int2buffer(17179869184), // total difficulty in genesis block
-      bestHash: Buffer.from(
-        'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
-        'hex'
-      ),
-      genesisHash: Buffer.from(
-        'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
-        'hex'
-      )
-    });
+  private async fetchLatestBlockHash(): Promise<Buffer> {
+    const latestBlock = await this.web3.eth.getBlock('latest');
+    return Buffer.from(latestBlock.hash.substring(2), 'hex');
+  }
+
+  private async sendStatus(eth: any) {
+    const message = DirectTxPool.NetworkStatusMessage.get(this.chainId);
+
+    if (message) {
+      message.bestHash = await this.fetchLatestBlockHash();
+      try {
+        eth.sendStatus(message);
+      } catch (error) {
+        this.logger.debug(`[p2p] Send status error ${error.message}`);
+      }
+    } else {
+      throw new Error(`[p2p] Status message not defined for ${this.chainId}`);
+    }
   }
 
   private peerAdded(peer: any) {
@@ -141,6 +215,7 @@ export default class DirectTxPool implements ITxPool {
   }
 
   private onTransaction(payload: any[]) {
+    this.logger.debug(`[p2p] Received ${payload.length} transactions`);
     try {
       for (const rawTx of payload) {
         const tx = new EthereumTx(rawTx);
@@ -172,5 +247,7 @@ export default class DirectTxPool implements ITxPool {
     this.rlpx.on('peer:added', (peer: any) => {
       this.peerAdded(peer);
     });
+
+    this.rlpx.on('error', (error: any) => this.logger.debug(`[p2p] rlpx error ${error.message}`));
   }
 }
